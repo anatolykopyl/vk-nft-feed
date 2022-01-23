d5cff0e7889f84c555fe8d139e095c7190ae73fb
Anatoly <akopyl@radner.ru>
2022-01-23 03:22:56 +0300

    Переместил получение постов в getPosts.js

--- getImages.js
+++ getImages.js
@@ -1,4 +1,3 @@
-import ProgressBar from 'progress';
 import getPosts from './getPosts.js';
 import post2Svg from './post2Svg.js';
 
@@ -16,41 +15,8 @@ function filterPosts(posts) {
   })
 }
 
-async function getAllPosts(owner_id) {
-  let finished = false;
-  let offset = 0;
-  let posts = [];
-  let bar;
-
-  while (!finished) {
-    const response = await getPosts(owner_id, offset);
-    const total = response.count;
-
-    if (!bar) {
-      bar = new ProgressBar(
-        'Getting posts [:bar] :current/:total :percent', 
-        { 
-          total,
-          width: 30,
-        }
-      );
-    }
-    bar.tick(response.items.length);
-
-    posts = posts.concat(response.items);
-    if (posts.length === total) {
-      finished = true;
-    } else {
-      offset += response.items.length;
-      await new Promise(r => setTimeout(r, 5000));
-    }
-  }
-
-  return posts;
-}
-
 export default async function(owner_id) {
-  let posts = await getAllPosts(owner_id);
+  let posts = await getPosts(owner_id);
   posts = filterPosts(posts);
 
   let images = [];
--- getPosts.js
+++ getPosts.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import ProgressBar from 'progress';
 
-export default async function(owner_id, offset) {
+ async function getPosts(owner_id, offset) {
   const res = await axios.get('https://api.vk.com/method/wall.get', {
     params: {
       access_token: process.env.SERVICE_KEY,
@@ -13,4 +14,37 @@ export default async function(owner_id, offset) {
   });
 
   return res.data.response;
+}
+
+export default async function getAllPosts(owner_id) {
+  let finished = false;
+  let offset = 0;
+  let posts = [];
+  let bar;
+
+  while (!finished) {
+    const response = await getPosts(owner_id, offset);
+    const total = response.count;
+
+    if (!bar) {
+      bar = new ProgressBar(
+        'Getting posts [:bar] :current/:total :percent', 
+        { 
+          total,
+          width: 30,
+        }
+      );
+    }
+    bar.tick(response.items.length);
+
+    posts = posts.concat(response.items);
+    if (posts.length === total) {
+      finished = true;
+    } else {
+      offset += response.items.length;
+      await new Promise(r => setTimeout(r, 5000));
+    }
+  }
+
+  return posts;
 }
